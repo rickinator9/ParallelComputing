e23637f83f200ff674403f3ae32646c304d75fe4
Jaron <joenewassenaar@hotmail.com>
2017-05-17 14:09:23 +0200

    changes to main file, made during class

--- SelectionSortAssignment/src/main.java
+++ SelectionSortAssignment/src/main.java
@@ -1,41 +1,14 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Rick on 20-4-2017.
  */
 public class main {
-    static final int SORT_COUNT = 5;
+    private static final int SORT_COUNT = 1;
+    private static EventProfiler profiler = new EventProfiler(true);;
 
-    public static void PrintArray(int[] array) {
-        System.out.print("Array: ");
-        System.out.print(array[0]);
 
-        for(int i = 1; i < array.length; i++) {
-            System.out.print(", ");
-            System.out.print(array[i]);
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        ISortingAlgorithm sort = new SelectionSort();
-
-        Benchmark[] benchmarks = new Benchmark[SORT_COUNT];
-        int[] toBeSorted = generateRandomDataSet(10000);
-        //PrintArray(toBeSorted);
-        for(int i = 0; i < SORT_COUNT; i++) {
-            ISortingAlgorithm.SortingResults sortingResults = sort.sort(toBeSorted);
-            int[] sorted = sortingResults.getSortedData();  // Note that this method returns a new array.
-                                                            // toBeSorted is not changed in any way.
-
-            //PrintArray(sorted);
-            benchmarks[i] = sortingResults.benchmark;
-            System.out.println("Dataset is sorted: " + isDataSetSorted(sorted));
-        }
-
-        Benchmark average = Benchmark.getAverage(benchmarks);
-        average.print();
-    }
 
     private static boolean isDataSetSorted(int[] dataSet) {
         for(int i = 0; i < dataSet.length-1; i++) {
@@ -46,8 +19,43 @@ public class main {
 
     private static int[] generateRandomDataSet(int numElements) {
         int[] dataSet = Utils.fillArray(numElements);
+        profiler.log("Filling array");
         Utils.shuffleArray(dataSet);
+        profiler.log("Shuffling array");
+        System.out.println(" ");
 
         return dataSet;
     }
+
+
+    public static void main(String[] args) {
+        ISortingAlgorithm sort = new SelectionSort();
+        Benchmark[] benchmarks = new Benchmark[SORT_COUNT];
+        List<Runnable> runners = new ArrayList<Runnable>();
+        int processors = Runtime.getRuntime().availableProcessors();
+        System.out.println("Processors " + processors);
+
+        profiler.start();
+
+        //A dataset will be generated and shuffled
+        int[] toBeSorted = generateRandomDataSet(10);
+
+        profiler.start();
+
+
+
+        for(int i = 0; i < SORT_COUNT; i++) {
+            ISortingAlgorithm.SortingResults sortingResults = sort.sort(toBeSorted);
+            profiler.log("Sorting");
+            int[] sorted = sortingResults.getSortedData();  // Note that this method returns a new array.
+                                                            // toBeSorted is not changed in any way.
+
+
+            System.out.println("Dataset is sorted: " + isDataSetSorted(sorted));
+            profiler.log("Checking sorted array");
+
+        }
+
+    }
+
 }
